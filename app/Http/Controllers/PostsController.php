b6a9980b77bd8813ba59ebb80de6ef31c294f7ea
yoshimitsuEgashira <yoshimitsu.egashira@circu.co.jp>
2019-12-10 14:32:17 +0900

    Impl pagination ( Fix #9 )

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -8,7 +8,7 @@ use App\Post;
 class PostsController extends Controller
 {
     public function index() {
-        $posts = Post::orderBy('created_at', 'desc')->get();
+        $posts = Post::orderBy('created_at', 'desc')->paginate(10);
         return view('posts.index', ['posts' => $posts]);
     }
 
